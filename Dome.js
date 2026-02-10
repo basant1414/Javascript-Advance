574837adc5773da6aeb3e2f13acc2d9b0d70ee5f
Basant <bhattbasant523@gmail.com>
2026-02-10 09:01:16 +0545

    DOM, DOM Manupulation, DOM Tree Nodes study

---
+++ Dome.js
@@ -0,0 +1,36 @@
+//dom document object model, window object vitra document object le html lai javascript bata access garnu nai dom ho
+// dom is required to perform dynamic changes in the program
+
+//DOM maniupulation
+
+//scirpt tag lai body tag vanda agi lekhe dom manipulation garna sakidaina
+
+/* DOM tree Nodes
+1. text nodes
+2. comment nodes
+3. element noes  */
+
+//selecting with id
+document.getElementById();
+
+//selecting with classname
+document.getElementsByClassName(); //collection is simillar to array bu tnot array
+
+//selecting with tag
+document.getElementsByTagName();
+
+//Query selector
+
+document.querySelector(); // we can pass tag, className & ID in it
+//returns first element
+
+document.querySelectorAll(); //returns node list, and all elements
+
+//classname inside queryselector is written with . e.g (".container")
+
+//Properties
+// on the basis of properties we can set,get, update and change the value
+/*TagName;
+innerText;
+innerHTML;
+textContent; */
