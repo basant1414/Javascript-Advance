e0e10805c54b39630c6b5aab3b47bb928da9a8c9
Basant <bhattbasant523@gmail.com>
2026-02-23 08:58:20 +0545

    Array methods pop(), Push(), Length practice

---
+++ Assignments/Arrays/Level01.js
@@ -0,0 +1,13 @@
+const arr  = [1,2,4,12,14]
+// Q1. remove the last item of the array, 
+// hint use pop() method
+//console.log(arr.pop())
+
+// Q2. Add new value 55 to the end of the array, hint use push() method
+
+console.log(arr.push(55))
+console.log(arr) //[1, 2, 4, 12, 55] it will put number at last place , 
+// place the  value to last of array, what you push
+
+// Q3. Find the length of the array, expected output is 4
+console.log(arr.length)
